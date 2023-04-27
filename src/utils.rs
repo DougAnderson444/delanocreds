246fb0270cbd8ac142e68bae2634341c662c90d6
Doug A <douganderson444@gmail.com>
2023-04-27 07:09:58 -0300

    add Pedersen utils

--- src/utils.rs
+++ src/utils.rs
@@ -1,11 +1,9 @@
-use amcl_wrapper::field_elem::{FieldElement, FieldElementVector};
+use amcl_wrapper::field_elem::FieldElement;
 use amcl_wrapper::group_elem::GroupElement;
 use amcl_wrapper::group_elem_g1::G1;
-use amcl_wrapper::group_elem_g2::G2;
-use amcl_wrapper::types::BigNum;
 use amcl_wrapper::univar_poly::UnivarPolynomial;
-use amcl_wrapper::univar_polynomial;
-#[derive(Clone, Debug)]
+
+#[derive(Clone, Debug, Eq, Hash, PartialEq)]
 pub enum InputType {
     String(String),
     VecString(Vec<String>),
@@ -45,8 +43,56 @@ pub fn polyfromroots(coeffs: Vec<FieldElement>) -> UnivarPolynomial {
 /// Sum EC points list.
 pub fn ec_sum(listpoints: Vec<G1>) -> G1 {
     let mut sum = G1::identity();
-    for i in 0..listpoints.len() {
-        sum += &listpoints[i];
+    for pts in &listpoints {
+        sum += pts;
     }
     sum
+}
+
+pub struct Pedersen {
+    pub g: G1,
+    pub h: G1,
+    trapdoor: FieldElement,
+}
+pub type Trapdoor = FieldElement;
+pub type PedersenCommit = G1;
+
+#[derive(Clone, Debug, Eq, Hash, PartialEq)]
+pub struct PedersenOpen {
+    pub open_randomness: FieldElement,
+    pub announce_randomness: FieldElement,
+    pub announce_element: Option<G1>,
+}
+
+impl PedersenOpen {
+    pub fn element(&mut self, elem: G1) {
+        self.announce_element = Some(elem);
+    }
+}
+
+impl Pedersen {
+    pub fn new() -> Self {
+        let g = G1::generator();
+        let d = FieldElement::random();
+        let h = &d * &g;
+        Pedersen { g, h, trapdoor: d }
+    }
+    pub fn commit(&self, msg: FieldElement) -> (PedersenCommit, PedersenOpen) {
+        let r = FieldElement::random();
+        let pedersen_commit = &r * &self.h + &msg * &self.g;
+        let pedersen_open = PedersenOpen {
+            open_randomness: r,
+            announce_randomness: msg,
+            announce_element: None,
+        };
+
+        (pedersen_commit, pedersen_open)
+    }
+
+    /// Decrypts/Decommit the message
+    pub fn decommit(&self, pedersen_open: &PedersenOpen, pedersen_commit: &PedersenCommit) -> bool {
+        let c2 = pedersen_open.open_randomness.clone() * &self.h
+            + pedersen_open.announce_randomness.clone() * &self.g;
+        &c2 == pedersen_commit
+    }
 }
