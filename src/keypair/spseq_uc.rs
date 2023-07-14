b3579f8c041ed38fc06c184deff2c6427b69f29f
Doug A <douganderson444@gmail.com>
2023-07-14 18:03:08 -0300

    fixes, mv change_rel to spseq

--- src/keypair/spseq_uc.rs
+++ src/keypair/spseq_uc.rs
@@ -44,7 +44,7 @@ impl From<SerzDeserzError> for UpdateError {
 /// update key is provided.
 /// - `vk` [`VK`] is the verification key used in the signature
 ///
-#[derive(Clone)]
+#[derive(Clone, Debug, PartialEq)]
 pub struct Credential {
     pub sigma: Signature,
     pub update_key: UpdateKey, // Called DelegatableKey (dk for k prime) in the paper
@@ -60,12 +60,9 @@ pub struct Credential {
 /// The update key is computed during the signing process.
 ///
 /// # Arguments
-/// - `vk`: the verification key
-/// - `pk_u`: the user public key
-/// - `commitment_vector`: the commitment vector
-/// - `opening_vector`: opening information vector related to commitment vector
-/// - `sigma`: the signature
-/// - `mu`: randomness is used to randomize commitment vector and signature accordingly
+/// - `pk_u`: Current public key associated to the Credential's Sigma Signature
+/// - `cred`: [Credential] to be updated
+/// - `mu`: randomness is used to randomize commitment vector and signature accordingly. Should be set to one (1) unless it's the final use, as it cannot be re-randomized again.
 /// - `psi`: randomness is used to randomize commitment vector and signature accordingly
 /// - `extendable`: a flag to determine if it needs to refresh (randomize) the `update_key` as well or not. Only takes
 /// effect if there is both `b` and an `orig_sig.update_key`
@@ -73,9 +70,8 @@ pub struct Credential {
 /// # Returns
 /// returns an updated signature σ for a new commitment vector and corresponding openings
 pub fn change_rep(
-    vk: &[VK],
     pk_u: &G1,
-    orig_sig: &Credential,
+    cred: &Credential,
     mu: &FieldElement,
     psi: &FieldElement,
     extendable: bool,
@@ -84,18 +80,18 @@ pub fn change_rep(
     let chi = FieldElement::random();
 
     // randomize Commitment and opening vectors and user public key with randomness mu, chi
-    let rndmz_commit_vector: Vec<G1> = orig_sig.commitment_vector.iter().map(|c| mu * c).collect();
+    let rndmz_commit_vector: Vec<G1> = cred.commitment_vector.iter().map(|c| mu * c).collect();
 
     let rndmz_opening_vector: Vec<FieldElement> =
-        orig_sig.opening_vector.iter().map(|o| mu * o).collect();
+        cred.opening_vector.iter().map(|o| mu * o).collect();
 
     // Randomize public key with two given randomness psi and chi.
     let rndmz_pk_u = psi * &(pk_u + &chi * &G1::generator());
 
     // adapt the signature for the randomized commitment vector and PK_u_prime
-    let Signature { z, y_g1, y_hat, t } = &orig_sig.sigma;
+    let Signature { z, y_g1, y_hat, t } = &cred.sigma;
 
-    if let VK::G1(vk0) = &vk[0] {
+    if let VK::G1(vk0) = &cred.vk[0] {
         let sigma_prime = Signature {
             z: mu * &psi.inverse() * z,
             y_g1: psi * y_g1,
@@ -103,21 +99,21 @@ pub fn change_rep(
             t: psi * &(t + &chi * vk0),
         };
 
-        // randomize update key with randomness mu
-        let mut fresh_update_key = None;
-        if extendable {
-            if let Some(update_key) = &orig_sig.update_key {
+        // randomize update key with randomness mu, psi
+        let fresh_update_key = match &cred.update_key {
+            Some(usign) if extendable => {
                 let mut usign_prime = Vec::new();
-                usign_prime.resize(update_key.len(), Vec::new());
-                for k in orig_sig.commitment_vector.len() + 1..update_key.len() {
-                    usign_prime[k - 1] = update_key[k - 1]
+                usign_prime.resize(usign.len(), Vec::new());
+                for k in cred.commitment_vector.len()..usign.len() {
+                    usign_prime[k] = usign[k]
                         .iter()
                         .map(|item| mu * &psi.inverse() * item)
                         .collect();
                 }
-                fresh_update_key = Some(usign_prime);
+                Some(usign_prime)
             }
-        }
+            _ => None,
+        };
 
         (
             RandomizedPubKey(rndmz_pk_u),
@@ -126,12 +122,88 @@ pub fn change_rep(
                 update_key: fresh_update_key,
                 commitment_vector: rndmz_commit_vector,
                 opening_vector: rndmz_opening_vector,
-                vk: vk.to_vec(),
+                vk: cred.vk.to_vec(),
             },
             chi,
         )
     } else {
         panic!("Invalid verification key");
+    }
+}
+
+/// Change Relations of a [Credential]. Push additional [Entry] onto the end of Message Commitment Stack.
+///
+/// Appends new randomized commitment and opening for the new entry.
+///
+/// Updates the signature for a new commitment vector including 𝐶_L for message_l using update_key
+///
+/// Referred to as `change_rel` or "Change Relations" in the paper.
+///
+/// # Arguments
+/// - `message_l`: message set at index `index_l` that will be added in message vector
+/// - `index_l`: index of `update_key` to be used for the added element,
+///             `[1..n]` (starts at 1)
+/// - `signature`: EqcSignature {sigma, update_key, commitment_vector, opening_vector}
+/// - `mu`: optional randomness, default to 1. Only applies when same randomness is used previosuly in [`change_rep`] function
+///
+/// # Returns
+/// new signature including the message set at index l
+pub fn change_rel(
+    parameters: &ParamSetCommitment,
+    addl_attrs: &Entry,
+    orig_sig: Credential,
+    mu: &FieldElement,
+) -> Result<Credential, UpdateError> {
+    // Validate the input. There must be room between the length of the current commitment vector
+    // and the length of the update key to append a new entry.
+    // valid input if: index_l = orig_sig.commitment_vector.len() + 1 && orig_sig.commitment_vector.len() + 1 <= orig_sig.update_key.as_ref().unwrap().len()
+    let index_l = orig_sig.commitment_vector.len();
+
+    match &orig_sig.update_key {
+        // can only change attributes if we have the messages and an update_key
+        Some(usign) if index_l < usign.len() => {
+            let Signature { z, y_g1, y_hat, t } = orig_sig.sigma;
+            let (commitment_l, opening_l) = encode(parameters, addl_attrs)?;
+
+            let rndmz_commitment_l = mu * &commitment_l;
+            let rndmz_opening_l = mu * &opening_l;
+
+            let set_l = convert_entry_to_bn(addl_attrs)?;
+            let monypolcoefficient = UnivarPolynomial::new_with_roots(&set_l[..]);
+
+            let list = usign.get(index_l).unwrap();
+            let sum_points_uk_i = list
+                .iter()
+                .zip(monypolcoefficient.coefficients().iter())
+                .fold(G1::identity(), |acc, (list_i, monypolcoefficient_i)| {
+                    acc + list_i.scalar_mul_const_time(monypolcoefficient_i)
+                });
+
+            let gama_l = sum_points_uk_i.scalar_mul_const_time(&opening_l);
+
+            let z_tilde = z + &gama_l;
+
+            let sigma_tilde = Signature {
+                z: z_tilde,
+                y_g1,
+                y_hat,
+                t,
+            };
+
+            let mut commitment_vector_tilde = orig_sig.commitment_vector;
+            commitment_vector_tilde.push(rndmz_commitment_l);
+
+            let mut opening_vector_tilde = orig_sig.opening_vector;
+            opening_vector_tilde.push(rndmz_opening_l);
+
+            Ok(Credential {
+                sigma: sigma_tilde,
+                commitment_vector: commitment_vector_tilde,
+                opening_vector: opening_vector_tilde,
+                ..orig_sig
+            })
+        }
+        _ => Err(UpdateError::Error),
     }
 }
 
