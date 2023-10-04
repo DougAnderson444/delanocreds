f4e50abac7ffe69918a9078d03de8b0e0737067a
DougAnderson444 <douganderson444@gmail.com>
2023-10-04 16:46:09 -0300

    autocomplete / password saving

--- crates/delano-leptos-site/src/app/routes/home.rs
+++ crates/delano-leptos-site/src/app/routes/home.rs
@@ -1,3 +1,4 @@
+use leptos::ev::SubmitEvent;
 use leptos::{leptos_dom::helpers::location_hash, *};
 use leptos_router::unescape;
 use leptos_router::*;
@@ -33,13 +34,13 @@ pub(crate) fn Home(cx: Scope) -> impl IntoView {
                         // No label, so show create screen
                         log::info!("Hash: {:?} but no label yet", h);
                         // active_view.update(|v| *v = view! {cx, <Splash /> })
-                        view! {cx, <Splash /> }
+                        view! { cx, <Splash/> }
                     }
                     lap => {
                         // There is a label, so use it to create the key
                         log::info!("Label {:?} and Hash: {:?}", lap.label, h);
                         //active_view.update(|v| *v = view! {cx, <CreateKey label_and_pin=lap /> })
-                        view! {cx, <CreateKey label_and_pin=lap /> }
+                        view! { cx, <CreateKey label_and_pin=lap/> }
                     }
                 }
             }
@@ -49,13 +50,13 @@ pub(crate) fn Home(cx: Scope) -> impl IntoView {
                         // No label, so show create screen
                         log::info!("No Hash, no label bro");
                         //active_view.update(|v| *v = view! {cx, <Splash /> })
-                        view! {cx, <Splash /> }
+                        view! { cx, <Splash/> }
                     }
                     lap => {
                         // There is a label, so use it to create the key
                         log::info!("No hash, new Label {:?}", lap.label);
                         // active_view.update(|v| *v = view! {cx, <CreateKey label_and_pin=lap /> })
-                        view! {cx, <CreateKey label_and_pin=lap /> }
+                        view! { cx, <CreateKey label_and_pin=lap/> }
                     }
                 }
             }
@@ -69,25 +70,30 @@ pub(crate) fn Home(cx: Scope) -> impl IntoView {
 pub fn Splash(cx: Scope) -> impl IntoView {
     let (count, set_count) = create_signal(cx, 0);
 
-    view! {
-        cx,
+    view! { cx,
         <div class="my-0 mx-auto max-w-3xl text-center">
-            <h2 class="p-6 text-4xl font-bold">"Delanocreds."</h2>
             <List>
                 <p>
                     "Create & Publish your"
-                    <code class="text-sm font-bold text-gray-900 bg-green-200 p-2 ml-1 rounded">"Verification Key"</code>
+                    <code class="text-sm font-bold text-gray-900 bg-green-200 p-2 ml-1 rounded">
+                        "Verification Key"
+                    </code>
                 </p>
                 <p>
                     "Issue some"
-                    <code class="text-sm font-bold text-gray-900 bg-green-200 p-2 ml-1 rounded">"Credentials"</code>
+                    <code class="text-sm font-bold text-gray-900 bg-green-200 p-2 ml-1 rounded">
+                        "Credentials"
+                    </code>
                 </p>
-                <p>"Send them to your"
-                    <code class="text-sm font-bold text-gray-900 bg-green-200 p-2 ml-1 rounded">"Contacts"</code>
+                <p>
+                    "Send them to your"
+                    <code class="text-sm font-bold text-gray-900 bg-green-200 p-2 ml-1 rounded">
+                        "Contacts"
+                    </code>
                 </p>
             </List>
             <div class="flex flex-col items-center justify-center p-2">
-                <PinPad />
+                <PinPad/>
             </div>
         </div>
     }
@@ -110,8 +116,17 @@ pub fn PinPad(cx: Scope) -> impl IntoView {
     let setter =
         use_context::<WriteSignal<LabelAndPin>>(cx).expect("to have found the setter provided");
 
-    view! { cx,
+    let on_submit = move |ev: SubmitEvent| {
+        ev.prevent_default();
+        setter.update(|value| {
+            *value = LabelAndPin {
+                label: label.get().to_owned(),
+                pin: pin.get().into(),
+            }
+        });
+    };
 
+    view! { cx,
         <div class="flex flex-col items-center justify-center">
             <div class="text-2xl font-sans tracking-tight items-center justify-center">"Label"</div>
             <input
@@ -119,10 +134,6 @@ pub fn PinPad(cx: Scope) -> impl IntoView {
                 class="p-2 my-2 border rounded-lg w-full bg-gray-50"
                 type="text"
                 on:input=move |ev| {
-                    // event_target_value is a Leptos helper function
-                    // it functions the same way as event.target.value
-                    // in JavaScript, but smooths out some of the typecasting
-                    // necessary to make this work in Rust
                     set_label(event_target_value(&ev));
                 }
 
@@ -133,54 +144,86 @@ pub fn PinPad(cx: Scope) -> impl IntoView {
         </div>
 
         // <!-- only show the last Number, all other leading numbers convert to * -->
-        <p>"Pin " {move || pin().chars().map(
-            |c| if c == pin().chars().last().unwrap() {
-                c
-            } else {
-                '*'
-            }
-        ).collect::<String>()
-        }</p>
+        <p>
+            "Pin "
+            {move || {
+                pin()
+                    .chars()
+                    .map(|c| if c == pin().chars().last().unwrap() { c } else { '*' })
+                    .collect::<String>()
+            }}
+
+        </p>
         // note the on:click instead of on_click
         // this is the same syntax as an HTML element event listener
         <div class="flex flex-col items-center justify-center">
             <div class="flex flex-row justify-between w-full space-x-2">
-                <PinButton label="1".to_string() on:click=move |_| set_pin.update(|value| *value = value.to_owned() + "1")/>
-                <PinButton label="2".to_string() on:click=move |_| set_pin.update(|value| *value = value.to_owned() + "2")/>
-                <PinButton label="3".to_string() on:click=move |_| set_pin.update(|value| *value = value.to_owned() + "3")/>
+                <PinButton
+                    label="1".to_string()
+                    on:click=move |_| set_pin.update(|value| *value = value.to_owned() + "1")
+                />
+                <PinButton
+                    label="2".to_string()
+                    on:click=move |_| set_pin.update(|value| *value = value.to_owned() + "2")
+                />
+                <PinButton
+                    label="3".to_string()
+                    on:click=move |_| set_pin.update(|value| *value = value.to_owned() + "3")
+                />
             </div>
             <div class="flex flex-row justify-between w-full space-x-2">
-                <PinButton label="4".to_string() on:click=move |_| set_pin.update(|value| *value = value.to_owned() + "4")/>
-                <PinButton label="5".to_string() on:click=move |_| set_pin.update(|value| *value = value.to_owned() + "5")/>
-                <PinButton label="6".to_string() on:click=move |_| set_pin.update(|value| *value = value.to_owned() + "6")/>
+                <PinButton
+                    label="4".to_string()
+                    on:click=move |_| set_pin.update(|value| *value = value.to_owned() + "4")
+                />
+                <PinButton
+                    label="5".to_string()
+                    on:click=move |_| set_pin.update(|value| *value = value.to_owned() + "5")
+                />
+                <PinButton
+                    label="6".to_string()
+                    on:click=move |_| set_pin.update(|value| *value = value.to_owned() + "6")
+                />
             </div>
             <div class="flex flex-row justify-between w-full space-x-2">
-                <PinButton label="7".to_string() on:click=move |_| set_pin.update(|value| *value = value.to_owned() + "7")/>
-                <PinButton label="8".to_string() on:click=move |_| set_pin.update(|value| *value = value.to_owned() + "8")/>
-                <PinButton label="9".to_string() on:click=move |_| set_pin.update(|value| *value = value.to_owned() + "9")/>
+                <PinButton
+                    label="7".to_string()
+                    on:click=move |_| set_pin.update(|value| *value = value.to_owned() + "7")
+                />
+                <PinButton
+                    label="8".to_string()
+                    on:click=move |_| set_pin.update(|value| *value = value.to_owned() + "8")
+                />
+                <PinButton
+                    label="9".to_string()
+                    on:click=move |_| set_pin.update(|value| *value = value.to_owned() + "9")
+                />
             </div>
             <div class="flex flex-row justify-between w-full space-x-2">
-                <div class="w-20" />
-                <PinButton label="0".to_string() on:click=move |_| set_pin.update(|value| *value = value.to_owned() + "0")/>
-                <div class="w-20" />
+                <div class="w-20"></div>
+                <PinButton
+                    label="0".to_string()
+                    on:click=move |_| set_pin.update(|value| *value = value.to_owned() + "0")
+                />
+                <div class="w-20"></div>
             </div>
-            <div class="flex flex-row justify-between w-full space-x-2">
-                <button
-                    class="bg-blue-500 w-full text-white px-4 py-2 my-1 rounded shadow"
-                    disabled={move || pin_too_short() || label_too_short()}
-                    on:click=move |_| setter.update(|value| {
-                        *value = LabelAndPin {
-                        label: label.get().to_owned(),
-                        pin: pin.get().into(),
-                    }})
-                    >{move || { button_label() }}
-                </button>
-            </div>
-
-            <Form method="GET" action="" >
-                <input type="password" name="pin" value={ move || pin.get() }  autocomplete="username" />
-                <input type="text" name="label" autocomplete="current-password" value={move || label.get()} />
-            </Form>
+            <form on:submit=on_submit>
+                <input
+                    type="password"
+                    name="pin"
+                    value=move || pin.get()
+                    autocomplete="current-password"
+                />
+                <input type="text" name="label" autocomplete="username" value=move || label.get()/>
+                <div class="flex flex-row justify-between w-full space-x-2">
+                    <input
+                        type="submit"
+                        value={move || { button_label() }}
+                        disabled=move || pin_too_short() || label_too_short()
+                        class="w-full  px-4 py-2 my-1 rounded shadow-lg disabled:bg-slate-50 bg-blue-500 disabled:text-slate-700 text-white"
+                    />
+                </div>
+            </form>
         </div>
     }
 }
@@ -188,9 +231,11 @@ pub fn PinPad(cx: Scope) -> impl IntoView {
 /// Pin pad button component
 #[component]
 pub fn PinButton(cx: Scope, label: String) -> impl IntoView {
-    view! {
-        cx,
-        <button type="number" class="px-4 py-2 my-1 rounded border w-full mb-2 bg-slate-100 border-slate-300" >
+    view! { cx,
+        <button
+            type="number"
+            class="px-4 py-2 my-1 rounded border w-full mb-2 bg-slate-100 border-slate-300"
+        >
             {label}
         </button>
     }
