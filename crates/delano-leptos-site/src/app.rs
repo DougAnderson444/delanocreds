42ccdabd8f8ff59beaf4c2a3056c3f51cb95e6cb
DougAnderson444 <douganderson444@gmail.com>
2023-10-05 10:26:50 -0300

    add constants for routes

--- crates/delano-leptos-site/src/app.rs
+++ crates/delano-leptos-site/src/app.rs
@@ -1,17 +1,23 @@
-// Where the Router routes are defined
+//! The Delanocreds app.
+
+/// Components available throughotu the app and routes:
+mod components;
+/// Constants module
+mod constants;
+/// Routes for the app path
 mod routes;
 /// Where Global State is structured
 mod state;
 
-mod list;
-mod screens;
-
 use leptos::*;
 use leptos_router::*;
 
+use constants::{ACCOUNT, HOME};
+use routes::authd::Authd;
 use routes::home::Home;
 use seed_keeper_core::Zeroizing;
 use state::*;
+
 /// The Label and Encrypted key params in the hash value
 ///
 /// `label` - A 6+ character string, usually a username, email, or phrase to identify the key
@@ -23,23 +29,27 @@ pub(crate) struct LabelAndPin {
 }
 
 #[component]
-pub fn App(cx: Scope) -> impl IntoView {
+pub fn App() -> impl IntoView {
     // Make the state available to all children
-    let (label_and_pin, set_label_n_pin) = create_signal(cx, LabelAndPin::default());
+    let (_, set_label_n_pin) = create_signal(LabelAndPin::default());
 
     // share the ability to set and read pin info to all children
-    provide_context(cx, set_label_n_pin);
+    provide_context(set_label_n_pin);
 
-    view! { cx,
+    view! {
         <Router>
             <nav>
                 <div class="my-0 mx-auto max-w-3xl text-center">
                     <h2 class="p-6 text-4xl font-bold">"Delanocreds."</h2>
                 </div>
             </nav>
-            <Routes>
-                <Route path="/delanocreds/" view=Home/>
-            </Routes>
+            <div class="p-2 my-0 mx-auto max-w-3xl">
+                <Routes>
+                    <Route path=HOME view=Home/>
+                    <Route path=ACCOUNT view=Authd />
+                    <Route path="/*any" view=|| view! { <h1>"Not Found"</h1> }/>
+                </Routes>
+            </div>
         </Router>
     }
 }
---
+++ crates/delano-leptos-site/src/app/constants/mod.rs
@@ -0,0 +1,4 @@
+// Route constants
+pub(crate) const HOME: &str = env!("BASE_PATH");
+/// ACCOUNT is env!("BASE_PATH") and "account"
+pub(crate) const ACCOUNT: &str = concat!(env!("BASE_PATH"), "account");
