e9a4876f7874a62242f1a0707fbc9b2002a6f2fe
DougAnderson444 <douganderson444@gmail.com>
2023-10-05 10:28:26 -0300

    make console-log optional for smaller binary size

--- crates/delano-leptos-site/src/main.rs
+++ crates/delano-leptos-site/src/main.rs
@@ -1,10 +1,22 @@
 mod app;
 
 use app::*;
+use cfg_if::cfg_if;
 use leptos::*;
 
-fn main() {
-    _ = console_log::init_with_level(log::Level::Debug);
+cfg_if! {
+    if #[cfg(feature = "console_log")] {
+        fn init_log() {
+            use log::Level;
+            console_log::init_with_level(log::Level::Debug).expect("log should initilize fine");
+        }
+    } else {
+        fn init_log() {}
+    }
+}
 
-    mount_to_body(|cx| view! { cx, <App/> })
+fn main() {
+    init_log();
+
+    mount_to_body(|| view! { <App/> })
 }
