4954f07a819187cda09378094d1fcc37e12435e3
Doug Anderson444 <douganderson444@gmail.com>
2023-10-10 07:43:23 -0300

    use serde feature of deps, formatting

--- crates/delano-leptos-site/src/app.rs
+++ crates/delano-leptos-site/src/app.rs
@@ -40,6 +40,7 @@ pub fn App() -> impl IntoView {
 
     view! {
         <Router>
+        <div class="relative flex min-h-screen flex-col justify-center overflow-hidden">
             <nav>
                 <div class="my-0 mx-auto max-w-3xl text-center">
                     <h2 class="p-6 text-4xl font-bold">"Delanocreds."</h2>
@@ -52,6 +53,7 @@ pub fn App() -> impl IntoView {
                     <Route path="/*any" view=|| view! { <h1>"Not Found"</h1> }/>
                 </Routes>
             </div>
+        </div>
         </Router>
     }
 }
--- crates/delano-leptos-site/src/app/routes/account.rs
+++ crates/delano-leptos-site/src/app/routes/account.rs
@@ -13,7 +13,7 @@ use leptos_router::unescape;
 /// Use Label and Pin to Create an encrypted key, and save it to the #hash part of the URL
 #[component]
 pub(crate) fn Account() -> impl IntoView {
-    // use the seed to derive a keypair for BLS12-38 use, using delano-keys, manager, from_seed
+    // use the seed to derive a keypair for BLS12-381 use, using delano-keys, manager, from_seed
     // read account from Global State. If use_context::<ReadSignal<ManagerState>>() returns some
     // manager, use it and continue to view this page. If not, use_navigate to redirect to Home page
     log::debug!("Account page");
@@ -32,7 +32,7 @@ pub(crate) fn Account() -> impl IntoView {
         let dest = format!("{HOME}#{hash}");
         log::debug!("No manager, redirecting to Home page {dest}");
         navigate(&dest, Default::default());
-        view! { "Navigating home..."}.into_view()
+        view! { "Navigating home..." }.into_view()
     } else {
         // if some, continue
         let m = maybe_manager.get().unwrap();
@@ -42,16 +42,19 @@ pub(crate) fn Account() -> impl IntoView {
         let expanded = account.expand_to(MaxEntries::default().into());
 
         // now use these secret keys in Issuer
-        let _issuer = Issuer::new_with_secret(expanded, MaxCardinality::default());
+        let issuer = Issuer::new_with_secret(expanded, MaxCardinality::default());
 
-        let pk_g1 = account.pk_g1.to_bytes();
-
-        let vk_g1_b64 = general_purpose::URL_SAFE_NO_PAD.encode(pk_g1);
+        let vk_g1_b64 = general_purpose::URL_SAFE_NO_PAD.encode(account.pk_g1.to_bytes());
+        let vk_g2_b64 = general_purpose::URL_SAFE_NO_PAD.encode(account.pk_g2.to_bytes());
 
         view! {
-            <div class="font-mono">
-                "Let's Create some Credentials! Using account public keys (G1 and G2):"
-                <pre class="font-mono">{vk_g1_b64.to_string()}</pre>
+            <div class="mx-auto max-w-md">
+                <div class="">
+                    "Let's Create some Credentials! Using account public info (G1, G2, Public Parameters):"
+                    <div class="mt-4 font-mono break-all">{vk_g1_b64.to_string()}.{vk_g2_b64}</div>
+                </div>
+                <div class="mt-4 font-semibold">"Your Public Parameters:"</div>
+                <div class="mt-4 font-mono break-all text-sm">{issuer.public.to_string()}</div>
             </div>
         }
         .into_view()
