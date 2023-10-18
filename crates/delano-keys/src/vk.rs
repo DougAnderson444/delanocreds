a84e4d4afa39aadace3c50bb18cf8fe69856936f
Doug Anderson444 <douganderson444@gmail.com>
2023-10-18 10:24:53 -0300

    add VK Compressed and de/serializable

--- crates/delano-keys/src/vk.rs
+++ crates/delano-keys/src/vk.rs
@@ -1,14 +1,107 @@
-//! The Verification key is made up of a VK::G1 as the first element, and VK::G2 as subsequent. Due
-//! to this fact, we need an enum to represent the VK.
+//! The Verification Key ([VK]) is made up of a [VK::G1] as the first element, and [VK::G2] as subsequent. Due
+//! to this fact, we need an enum to represent the [VK].
 
+use blastkids::GroupEncoding;
+use bls12_381_plus::{G1Affine, G1Compressed, G2Affine, G2Compressed};
 use bls12_381_plus::{G1Projective, G2Projective};
 
-/// Verification Key
-/// This key has elements from both G1 and G2,
+/// Verification Key [VK]
+/// This key has elements from both [G1Projective] and [G2Projective],
 /// so to make a Vector of [VK], we need to use enum
 #[derive(Debug, Clone, PartialEq)]
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 pub enum VK {
     G1(G1Projective),
     G2(G2Projective),
+}
+
+/// A Verification Key [VK] in compressed form as [VKCompressed]
+#[derive(Debug, Clone, PartialEq)]
+pub enum VKCompressed {
+    G1(G1Compressed),
+    G2(G2Compressed),
+}
+
+/// Implement TryFrom<Vec<u8>> for Verification Key [VK]
+/// This is used to convert a Vec<u8> into a [VK] enum variant
+/// Helpful for deserializing from JSON Base64
+impl std::convert::TryFrom<Vec<u8>> for VK {
+    type Error = String;
+
+    fn try_from(v: Vec<u8>) -> Result<Self, Self::Error> {
+        match v.len() {
+            48 => {
+                let mut bytes = [0u8; 48];
+                bytes.copy_from_slice(&v[..]);
+                let g1_maybe = G1Affine::from_compressed(&bytes);
+
+                if g1_maybe.is_none().into() {
+                    return Err("Invalid G1 point".to_string());
+                }
+
+                Ok(VK::G1(
+                    g1_maybe.expect("it'll be fine, it passed the check").into(),
+                ))
+            }
+            96 => {
+                let mut g2_bytes = [0u8; 96];
+                g2_bytes.copy_from_slice(&v[..]);
+                let g2 = G2Affine::from_compressed(&g2_bytes);
+
+                if g2.is_none().into() {
+                    return Err("Invalid G2 point".to_string());
+                }
+
+                Ok(VK::G2(
+                    g2.expect("it'll be fine, it passed the check").into(),
+                ))
+            }
+            _ => Err("Invalid Verification Key (VK) length".to_string()),
+        }
+    }
+}
+
+/// Implement AsRef<u8> for Verification Key compressed [VKCompressed]
+impl AsRef<[u8]> for VKCompressed {
+    fn as_ref(&self) -> &[u8] {
+        match self {
+            VKCompressed::G1(g1) => g1.as_ref(),
+            VKCompressed::G2(g2) => g2.as_ref(),
+        }
+    }
+}
+
+/// Impl From<Vec<u8>> for [VKCompressed]
+impl From<Vec<u8>> for VKCompressed {
+    fn from(v: Vec<u8>) -> Self {
+        match v.len() {
+            48 => {
+                let mut bytes = [0u8; 48];
+                bytes.copy_from_slice(&v[..]);
+                let g1_maybe = G1Affine::from_compressed(&bytes);
+
+                if g1_maybe.is_none().into() {
+                    panic!("Invalid G1 point");
+                }
+
+                VKCompressed::G1(
+                    g1_maybe
+                        .expect("it'll be fine, it passed the check")
+                        .to_bytes(),
+                )
+            }
+            96 => {
+                let mut g2_bytes = [0u8; 96];
+                g2_bytes.copy_from_slice(&v[..]);
+                let g2 = G2Affine::from_compressed(&g2_bytes);
+
+                if g2.is_none().into() {
+                    panic!("Invalid G2 point");
+                }
+
+                VKCompressed::G2(g2.expect("it'll be fine, it passed the check").to_bytes())
+            }
+            _ => panic!("Invalid VK length"),
+        }
+    }
 }
