53063a9687c5d35b93bff566a5350d2a90f80db7
Doug Anderson444 <douganderson444@gmail.com>
2023-10-18 10:21:48 -0300

    add library level error types

--- crates/delanocreds/src/error.rs
+++ crates/delanocreds/src/error.rs
@@ -1,1 +1,11 @@
-// pub enum Error
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum Error {
+    // Credential Signature is not valid for the new Nym
+    #[error("Invalid Signature")]
+    InvalidSignature(String),
+    // Change Relations Failed
+    #[error("Change Relations Failed")]
+    ChangeRelationsFailed(String),
+}
