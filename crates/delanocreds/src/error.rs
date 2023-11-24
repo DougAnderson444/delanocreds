c4462ac08e014b29e6eff184d9a602b0c816fc6a
Doug Anderson444 <douganderson444@gmail.com>
2023-11-24 08:17:09 -0400

    add several crate level error types with thiserror

--- crates/delanocreds/src/error.rs
+++ crates/delanocreds/src/error.rs
@@ -3,14 +3,14 @@ use thiserror::Error;
 #[derive(Error, Debug)]
 pub enum Error {
     // Credential Signature is not valid for the new Nym
-    #[error("Invalid Signature")]
+    #[error("Invalid Signature {0}")]
     InvalidSignature(String),
     // Change Relations Failed
-    #[error("Change Relations Failed")]
+    #[error("Change Relations Failed, {0}")]
     ChangeRelationsFailed(String),
 
     /// Failed to accept a [Credential] offer]
-    #[error("Failed to accept a Credential offer")]
+    #[error("Failed to accept a Credential offer {0}")]
     AcceptOfferFailed(String),
 
     /// Failed to create a [Credential] offer
@@ -33,4 +33,14 @@ pub enum Error {
     /// Tried to convert bytes into a Scalar and it failed
     #[error("Tried to convert bytes into a Scalar and it failed")]
     InvalidScalar,
+
+    #[error("Tried to apply CBOR coded, but failed {0}")]
+    CBORError(String),
+
+    /// From base64::DecodeError
+    #[error("From base64::DecodeError")]
+    Base64DecodeError(#[from] base64::DecodeError),
+
+    #[error("The given Nonce bytes were not convertable to Scalar")]
+    NonceConversionError,
 }
