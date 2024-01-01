53409927f623a608d897192bdbe6bd374218bad3
Doug Anderson444 <douganderson444@gmail.com>
2024-01-01 10:19:22 -0400

    start delano wit User Interface

---
+++ crates/delano-wit-ui/README.md
@@ -0,0 +1,15 @@
+# Delano User Interface
+
+A default implementation of the Delano UI using [`wurbo`](https://github.com/DougAnderson444/wurbo).
+
+Don't like the look of it? You will be able to fork it and modify it to your liking.
+
+# Usage
+
+This crate is just the User Interface to interact with the [`delano-wit`](../delano-wit/) Application Binary Interface (ABI) and display the resulting data.
+
+To use this crate, you will need to provide [`delano-wit`](../delano-wit/) (or your fork of it) as an import dependency when using `wasm-tools` to compose your app together.
+
+A reference composed app is available here.
+
+
---
+++ crates/delano-wit-ui/tests/mod.rs
@@ -0,0 +1,123 @@
+mod bindgen {
+    wasmtime::component::bindgen!("delanocreds-wit-ui"); // name of the world in the .wit file
+}
+
+use std::{
+    env,
+    path::{Path, PathBuf},
+};
+use thiserror::Error;
+use wasmtime::component::{Component, Linker};
+use wasmtime::{Config, Engine, Store};
+use wasmtime_wasi::preview2::{Table, WasiCtx, WasiCtxBuilder, WasiView};
+
+struct MyCtx {
+    wasi_ctx: Context,
+}
+
+struct Context {
+    table: Table,
+    wasi: WasiCtx,
+}
+impl WasiView for MyCtx {
+    fn table(&self) -> &Table {
+        &self.wasi_ctx.table
+    }
+    fn table_mut(&mut self) -> &mut Table {
+        &mut self.wasi_ctx.table
+    }
+    fn ctx(&self) -> &WasiCtx {
+        &self.wasi_ctx.wasi
+    }
+    fn ctx_mut(&mut self) -> &mut WasiCtx {
+        &mut self.wasi_ctx.wasi
+    }
+}
+
+// impl bindgen::delano::wallet::seed_keeper::Host for MyCtx {
+//     /// Stub a seed gen fn
+//     fn get_seed(&mut self) -> Result<Vec<u8>, wasmtime::Error> {
+//         Ok(vec![69u8; 32])
+//     }
+// }
+//
+// impl bindgen::delano::wallet::types::Host for MyCtx {}
+
+#[derive(Error, Debug)]
+pub enum TestError {
+    /// From String
+    #[error("Error message {0}")]
+    Stringified(String),
+
+    /// From Wasmtime
+    #[error("Wasmtime: {0}")]
+    Wasmtime(#[from] wasmtime::Error),
+
+    /// From VarError
+    #[error("VarError: {0}")]
+    VarError(#[from] std::env::VarError),
+
+    /// From io
+    #[error("IO: {0}")]
+    Io(#[from] std::io::Error),
+}
+
+/// Utility function to get the workspace dir
+pub fn workspace_dir() -> PathBuf {
+    let output = std::process::Command::new(env!("CARGO"))
+        .arg("locate-project")
+        .arg("--workspace")
+        .arg("--message-format=plain")
+        .output()
+        .unwrap()
+        .stdout;
+    let cargo_path = Path::new(std::str::from_utf8(&output).unwrap().trim());
+    cargo_path.parent().unwrap().to_path_buf()
+}
+
+#[cfg(test)]
+mod delano_wit_ui_tests {
+
+    use super::*;
+
+    #[test]
+    fn test_delano_wit_ui() -> wasmtime::Result<(), TestError> {
+        // get the target/wasm32-wasi/debug/CARGO_PKG_NAME.wasm file
+        let pkg_name = std::env::var("CARGO_PKG_NAME")?.replace('-', "_");
+        let workspace = workspace_dir();
+        let wasm_path = format!("target/wasm32-wasi/debug/{}.wasm", pkg_name);
+        let wasm_path = workspace.join(wasm_path);
+
+        let mut config = Config::new();
+        config.cache_config_load_default()?;
+        config.wasm_backtrace_details(wasmtime::WasmBacktraceDetails::Enable);
+        config.wasm_component_model(true);
+
+        let engine = Engine::new(&config)?;
+        let component = Component::from_file(&engine, &wasm_path)?;
+
+        let mut linker = Linker::new(&engine);
+
+        // link imports to our instantiation, if any
+        // since we don't have nay yet, we need to comment this out
+        // bindgen::DelanocredsWitUi::add_to_linker(&mut linker, |state: &mut MyCtx| state)?;
+
+        // link the WASI imports to our instantiation
+        wasmtime_wasi::preview2::command::sync::add_to_linker(&mut linker)?;
+
+        let table = Table::new();
+        let wasi: WasiCtx = WasiCtxBuilder::new().inherit_stdout().args(&[""]).build();
+        let state = MyCtx {
+            wasi_ctx: Context { table, wasi },
+        };
+        let mut store = Store::new(&engine, state);
+
+        let (bindings, _) =
+            bindgen::DelanocredsWitUi::instantiate(&mut store, &component, &linker)?;
+        // World
+
+        // Now let's make some credentials!
+
+        Ok(())
+    }
+}
