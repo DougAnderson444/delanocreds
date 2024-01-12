0c878a34a56277c99d965ccde6d764f70699be65
Doug Anderson444 <douganderson444@gmail.com>
2024-01-12 07:42:49 -0400

    rm leptos site
    in favour of the wit-ui

--- crates/delano-leptos-site/src/app/components/error.rs
+++
@@ -1,9 +0,0 @@
-use leptos::*;
-
-/// An Error component that shows a whoopsie message
-#[component]
-pub fn Error(message: String) -> impl IntoView {
-    view! {
-        <div class="bg-red-100 rounded p-2">"Whoops! "{message}</div>
-    }
-}
--- crates/delano-leptos-site/src/app/constants/mod.rs
+++
@@ -1,8 +0,0 @@
-// Route constants
-pub(crate) const HOME: &str = env!("BASE_PATH");
-/// ACCOUNT is env!("BASE_PATH") and "account"
-pub(crate) const ACCOUNT: &str = concat!(env!("BASE_PATH"), "account");
-/// OFFER is env!("BASE_PATH") and "offer"
-pub(crate) const OFFER: &str = "offer";
-/// For testing purposes only
-pub(crate) const TEST: &str = concat!(env!("BASE_PATH"), "test");
