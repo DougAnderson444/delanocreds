8633e1821194f95c0bd111b363a0bb8e7498e1ae
Doug Anderson444 <douganderson444@gmail.com>
2024-01-16 08:24:22 -0400

    use templates in lib

--- crates/delano-wit-ui/src/lib.rs
+++ crates/delano-wit-ui/src/lib.rs
@@ -22,13 +22,15 @@ use std::ops::Deref;
 
 struct Component;
 
+const INDEX_HTML: &str = "index.html";
 const ATTRIBUTES_HTML: &str = "attributes.html";
+const OUTPUT_HTML: &str = "output.html";
 
 /// We need to provide the templates for the macro to pull in
 fn get_templates() -> Templates {
     Templates::new(
-        Index::new("index.html", include_str!("templates/index.html")),
-        Entry::new("output.html", include_str!("templates/output.html")),
+        Index::new(INDEX_HTML, include_str!("templates/index.html")),
+        Entry::new(OUTPUT_HTML, include_str!("templates/output.html")),
         Rest::new(vec![
             Entry::new("input.html", include_str!("templates/input.html")),
             // "attributes.html"
@@ -49,6 +51,15 @@ struct StructContext {
     app: StructPage,
     issuer: IssuerStruct,
     output: OutputStruct,
+    target: Option<String>,
+}
+
+impl StructContext {
+    /// with this target
+    fn with_target(mut self, target: String) -> Self {
+        self.target = Some(target);
+        self
+    }
 }
 
 impl StructObject for StructContext {
@@ -74,15 +85,19 @@ impl From<&context_types::Context> for StructContext {
             }
             context_types::Context::Issuing(issuer) => {
                 StructContext::from(IssuerStruct::from(issuer))
+                    .with_target(ATTRIBUTES_HTML.to_string())
             }
             context_types::Context::Addattribute => {
                 StructContext::from(IssuerStruct::from_latest().push_attribute())
+                    .with_target(INDEX_HTML.to_string())
             }
             context_types::Context::Editissuerinput(kvctx) => StructContext::from(
-                StructContext::from(IssuerStruct::from_latest().edit_attribute(kvctx)),
+                StructContext::from(IssuerStruct::from_latest().edit_attribute(kvctx))
+                    .with_target(OUTPUT_HTML.to_string()),
             ),
             context_types::Context::Editissuermaxentries(max) => {
                 StructContext::from(StructContext::from(IssuerStruct::with_max_entries(max)))
+                    .with_target(OUTPUT_HTML.to_string())
             }
         }
     }
@@ -94,6 +109,7 @@ impl From<context_types::Everything> for StructContext {
             app: StructPage::from(context.page),
             issuer: IssuerStruct::from(context.issue),
             output: OutputStruct::default(),
+            target: None,
         }
     }
 }
@@ -103,15 +119,5 @@ impl From<IssuerStruct> for StructContext {
         let mut state = { LAST_STATE.lock().unwrap().clone().unwrap_or_default() };
         state.issuer = issuer_ctx.clone();
         state
-    }
-}
-
-impl From<&IssuerStruct> for StructContext {
-    fn from(context: &IssuerStruct) -> Self {
-        Self {
-            app: StructPage::from(None),
-            issuer: context.clone(),
-            output: OutputStruct::default(),
-        }
     }
 }
