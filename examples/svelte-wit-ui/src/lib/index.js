df8304af450a6a7e6491609851acdc16ae9133a9
Doug Anderson444 <douganderson444@gmail.com>
2024-01-19 18:25:06 -0400

    rm full for now

--- examples/svelte-wit-ui/src/lib/index.js
+++ examples/svelte-wit-ui/src/lib/index.js
@@ -1,6 +1,6 @@
 // place files you want to import through the `$lib` alias in this folder.
 import { default as UIOnly } from './ui/UIOnly.svelte';
-import { default as Full } from './full/FullWallet.svelte';
+// import { default as Full } from './full/FullWallet.svelte';
 
 export const examples = [
 	// Full,
