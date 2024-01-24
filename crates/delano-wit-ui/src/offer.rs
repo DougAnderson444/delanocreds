21802f3e5dc2ae2bd89cabc0199e5a181e88e6db
Doug Anderson444 <douganderson444@gmail.com>
2024-01-24 19:33:42 -0400

    move dummy to tests

--- crates/delano-wit-ui/src/offer.rs
+++ crates/delano-wit-ui/src/offer.rs
@@ -1,7 +1,6 @@
 //! Module to hold Offer code
 use super::*;
 
-use delanocreds::{Credential, Issuer, Nym};
 use serde::{Deserialize, Serialize};
 
 /// Offers will be sent to others over the wire, and injested by Wasm Interface types.
@@ -20,33 +19,33 @@ pub enum Context {
     },
 }
 
-/// A dummy Credential for testing and development
-/// Created from [delanocreds::Credential]
-pub fn dummy_cred() -> Credential {
-    let issuer = Issuer::default();
-    let nym = Nym::new();
-
-    let root_entry = delanocreds::Entry::new(&[]);
-    let nonce = delanocreds::Nonce::default();
-    // Issue the (Powerful) Root Credential to Alice
-    let cred = match issuer
-        .credential()
-        .with_entry(root_entry.clone())
-        .max_entries(&3)
-        .issue_to(&nym.nym_proof(&nonce), Some(&nonce))
-    {
-        Ok(cred) => cred,
-        Err(e) => panic!("Error issuing cred: {:?}", e),
-    };
-    cred
-}
-
 #[cfg(test)]
 mod tests {
     use delanocreds::CBORCodec;
+    use delanocreds::{Credential, Issuer, Nym};
 
     use super::*;
 
+    /// A dummy Credential for testing and development
+    /// Created from [delanocreds::Credential]
+    fn dummy_cred() -> Credential {
+        let issuer = Issuer::default();
+        let nym = Nym::new();
+
+        let root_entry = delanocreds::Entry::new(&[]);
+        let nonce = delanocreds::Nonce::default();
+        // Issue the (Powerful) Root Credential to Alice
+        let cred = match issuer
+            .credential()
+            .with_entry(root_entry.clone())
+            .max_entries(&3)
+            .issue_to(&nym.nym_proof(&nonce), Some(&nonce))
+        {
+            Ok(cred) => cred,
+            Err(e) => panic!("Error issuing cred: {:?}", e),
+        };
+        cred
+    }
     #[test]
     fn test_dummy_cred() {
         let cred = dummy_cred().to_bytes();
