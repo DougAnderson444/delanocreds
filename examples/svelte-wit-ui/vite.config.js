f179a86946805806af96be868449e087c6254482
Doug Anderson444 <douganderson444@gmail.com>
2024-02-01 16:46:52 -0400

    add vite config for wurbo 0.2.0

--- examples/svelte-wit-ui/vite.config.js
+++ examples/svelte-wit-ui/vite.config.js
@@ -2,5 +2,12 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	server: { fs: { strict: false } },
+	build: {
+		minify: false
+	},
+	worker: {
+		format: 'es'
+	}
 });
