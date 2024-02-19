53d3363ec4f9a56ecce69b90105948fa6a227da0
Doug Anderson444 <douganderson444@gmail.com>
2024-02-19 18:34:05 -0400

    add cbor to/from funcs

--- crates/delano-wit-ui/src/util.rs
+++ crates/delano-wit-ui/src/util.rs
@@ -1,3 +1,4 @@
+use serde::{Deserialize, Serialize};
 use std::fmt::Debug;
 
 use convert_case::{Case, Casing};
@@ -9,4 +10,27 @@ pub fn variant_string<T: Debug>(t: T) -> String {
         .split(|c: char| !c.is_alphanumeric())
         .collect::<Vec<_>>();
     parts[2].to_case(Case::Kebab)
+}
+
+/// Serializes to CBOR bytes
+pub(crate) fn try_cbor<T>(value: &T) -> Result<Vec<u8>, String>
+where
+    T: Sized + Serialize,
+{
+    let mut bytes = Vec::new();
+    match ciborium::into_writer(&value, &mut bytes) {
+        Ok(_) => Ok(bytes),
+        Err(e) => Err(format!("CBORCodec Error serializing to bytes: {}", e)),
+    }
+}
+
+/// Deserialize from CBOR bytes
+pub fn try_from_cbor<T>(bytes: &[u8]) -> Result<T, String>
+where
+    for<'a> T: Sized + Deserialize<'a>,
+{
+    match ciborium::from_reader(&bytes[..]) {
+        Ok(item) => Ok(item),
+        Err(e) => Err(format!("CBORCodec Error deserializing from bytes: {}", e)),
+    }
 }
