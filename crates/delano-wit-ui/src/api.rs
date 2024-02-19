40501d7a35ee3be68c292ec1881c80f9b9bcefc1
Doug Anderson444 <douganderson444@gmail.com>
2024-02-19 18:47:38 -0400

    use new wallet WIT API, add conversions

--- crates/delano-wit-ui/src/api.rs
+++ crates/delano-wit-ui/src/api.rs
@@ -5,15 +5,29 @@
 //! used to format and serialize the data as it's passed around.
 //!
 //! WIT interface types are kebab case, and all types must be serializable and deserializable.
-use self::attributes::{AttributeKOV, Hint};
+use self::{
+    attributes::{AttributeKOV, Hint},
+    util::{try_cbor, try_from_cbor},
+    wallet::types,
+};
 
 use super::*;
 
 use base64ct::{Base64UrlUnpadded, Encoding};
 use chrono::prelude::*;
 use delano_events::{Context, Provables, PublishMessage};
-use delano_keys::publish::{IssuerKey, OfferedPreimages, PublishingKey};
-use delanocreds::Attribute;
+use delano_keys::{
+    publish::{IssuerKey, OfferedPreimages, PublishingKey},
+    vk::VKCompressed,
+};
+use delanocreds::{
+    keypair::{
+        CredProofCompressed, IssuerPublicCompressed, NymProofCompressed, SignatureCompressed,
+    },
+    set_commits::ParamSetCommitmentCompressed,
+    zkp::{DamgardTransformCompressed, PedersenCompressed, PedersenOpenCompressed},
+    Attribute,
+};
 use serde::{Deserialize, Serialize};
 
 /// History is created when an offer is made, and is used to track the status of the offer.
@@ -23,7 +37,7 @@ pub(crate) struct History {
     /// The attributes of this offer
     pub(crate) attributes: Vec<Vec<AttributeKOV>>,
     /// The [delano_keys::VK] used to sign the offer
-    pub(crate) issuer_vk: Vec<Vec<u8>>,
+    pub(crate) issuer_vk: Vec<VKCompressed>,
     /// Publishing Key
     pub(crate) publish_key: Vec<u8>,
     /// The offer bytes as urlsafe base64
@@ -113,7 +127,7 @@ impl State {
                     .map_err(|e| format!("Issue failed in offer: {}", e))?;
                 let offer = self
                     .builder
-                    .offer(cred)
+                    .offer(&cred)
                     .map_err(|e| format!("Offer failed in offer: {}", e))?;
 
                 // convert the attributes to hint
@@ -128,7 +142,10 @@ impl State {
                     })
                     .collect::<Vec<_>>();
 
-                let offer = crate::api::Loaded::Offer { cred: offer, hints };
+                let offer = crate::api::Loaded::Offer {
+                    cred: try_cbor(&offer)?,
+                    hints,
+                };
 
                 // We also want to track this offer in the history
                 // So we can 1) Look up the key for values, and
@@ -141,9 +158,19 @@ impl State {
                 let Ok(issuer_key) = wallet::actions::issuer_public() else {
                     return Err("Issuer public key failed".to_string());
                 };
+
+                let issuer_vk = issuer_key
+                    .vk
+                    .iter()
+                    .map(|vk| match vk {
+                        types::VkCompressed::G1(g1) => VKCompressed::try_from(g1),
+                        types::VkCompressed::G2(g2) => VKCompressed::try_from(g2),
+                    })
+                    .collect::<Result<Vec<_>, _>>()?;
+
                 let publish_key = PublishingKey::new(
                     &delano_keys::publish::OfferedPreimages(&self.builder.entries),
-                    &delano_keys::publish::IssuerKey(&issuer_key),
+                    &delano_keys::publish::IssuerKey(&issuer_vk),
                 )
                 .cid();
 
@@ -153,9 +180,9 @@ impl State {
 
                 let history = History {
                     attributes: self.builder.entries.clone(),
-                    issuer_vk: issuer_key,
+                    issuer_vk,
                     publish_key: publish_key.into(),
-                    offer: offered.clone(),
+                    offer: offered.clone(), // TODO: Why would this need to be serialized and encoded??
                     timestamp: Utc::now().to_rfc3339(),
                 };
 
@@ -171,7 +198,7 @@ impl State {
     fn proof(&self) -> Result<Option<Loaded>, String> {
         match &self.loaded {
             Loaded::Offer { cred, .. } => {
-                let accepted = wallet::actions::accept(&cred)?;
+                let accepted = wallet::actions::accept(&try_from_cbor(cred)?)?;
                 let cred = self.builder.extend(accepted)?;
                 let proof_package = self.builder.proof_package(&cred)?;
                 match proof_package.verify() {
@@ -185,10 +212,10 @@ impl State {
         }
     }
 
-    pub(crate) fn with_cred(mut self, builder: CredentialStruct) -> Self {
-        self.builder = builder;
-        self
-    }
+    // pub(crate) fn with_cred(mut self, builder: CredentialStruct) -> Self {
+    //     self.builder = builder;
+    //     self
+    // }
 
     /// Publish the proof to the network by emiting a serialized message of the Key and Provables
     pub(crate) fn publish_proof(self) -> Self {
@@ -208,7 +235,7 @@ impl State {
         let publishables = delano_events::Publishables::new(
             PublishingKey::new(
                 &OfferedPreimages(&self.builder.entries[0]),
-                &IssuerKey(&issuer_key),
+                &IssuerKey(&issuer_key.vk.iter().map(|vk| vk.into()).collect::<Vec<_>>()),
             ),
             provables.clone(),
         );
@@ -344,8 +371,8 @@ impl Loaded {
 
                 // Step 2) Verify the proof using wallet::actions::verify
                 let verifiables = wallet::actions::Verifiables {
-                    proof: proof.clone(),
-                    issuer_public: issuer_public.clone(),
+                    proof: proof.into(),
+                    issuer_public: issuer_public.into(),
                     nonce: None,
                     selected: selected.clone(),
                 };
@@ -443,8 +470,7 @@ impl From<String> for Loaded {
 
 #[cfg(test)]
 mod tests {
-    use delanocreds::CBORCodec;
-    use delanocreds::{Credential, Issuer, Nym};
+    use delanocreds::{Credential, CredentialCompressed, Issuer, Nym};
 
     /// A dummy Credential for testing and development
     /// Created from [delanocreds::Credential]
@@ -468,9 +494,199 @@ mod tests {
     }
     #[test]
     fn test_dummy_cred() {
-        let cred = dummy_cred().to_bytes();
+        let cred = CredentialCompressed::from(dummy_cred());
 
         // print the bytes
-        println!("{:?}", cred);
+        println!("Compressed Dumy Cred {:?}", cred);
+    }
+}
+
+/// delanocreds::keypair::CredProofCompressed: From<types::CredProofCompressed>
+impl From<types::CredProofCompressed> for CredProofCompressed {
+    fn from(cred_proof: types::CredProofCompressed) -> Self {
+        CredProofCompressed {
+            sigma: cred_proof.sigma.into(),
+            commitment_vector: cred_proof.commitment_vector,
+            witness_pi: cred_proof.witness_pi,
+            nym_proof: cred_proof.nym_proof.into(),
+        }
+    }
+}
+
+/// delanocreds::keypair::SignatureCompressed: From<types::SignatureCompressed>
+impl From<types::SignatureCompressed> for SignatureCompressed {
+    fn from(signature: types::SignatureCompressed) -> Self {
+        SignatureCompressed {
+            z: signature.z,
+            y_g1: signature.y_g1,
+            y_hat: signature.y_hat,
+            t: signature.t,
+        }
+    }
+}
+
+/// NymProofCompressed: From<types::NymProofCompressed>
+impl From<types::NymProofCompressed> for NymProofCompressed {
+    fn from(nym_proof: types::NymProofCompressed) -> Self {
+        NymProofCompressed {
+            challenge: nym_proof.challenge,
+            pedersen_open: nym_proof.pedersen_open.into(),
+            pedersen_commit: nym_proof.pedersen_commit,
+            public_key: nym_proof.public_key,
+            response: nym_proof.response,
+            damgard: nym_proof.damgard.into(),
+        }
+    }
+}
+
+/// delanocreds::zkp::DamgardTransformCompressed: From<types::DamgardTransformCompressed>
+impl From<types::DamgardTransformCompressed> for DamgardTransformCompressed {
+    fn from(damgard: types::DamgardTransformCompressed) -> Self {
+        DamgardTransformCompressed {
+            pedersen: damgard.pedersen.into(),
+        }
+    }
+}
+
+/// delanocreds::zkp::PedersenCompressed: From<types::PedersenCompressed>
+impl From<types::PedersenCompressed> for PedersenCompressed {
+    fn from(pedersen: types::PedersenCompressed) -> Self {
+        PedersenCompressed::from(pedersen.h)
+    }
+}
+
+/// PedersenOpenCompressed: From<types::PedersenOpenCompressed>
+impl From<types::PedersenOpenCompressed> for PedersenOpenCompressed {
+    fn from(pedersen_open: types::PedersenOpenCompressed) -> Self {
+        PedersenOpenCompressed {
+            open_randomness: pedersen_open.open_randomness,
+            announce_randomness: pedersen_open.announce_randomness,
+            announce_element: pedersen_open.announce_element,
+        }
+    }
+}
+
+/// IssuerPublicCompressed: From<types::IssuerPublicCompressed>
+impl From<types::IssuerPublicCompressed> for IssuerPublicCompressed {
+    fn from(issuer_public: types::IssuerPublicCompressed) -> Self {
+        IssuerPublicCompressed {
+            parameters: issuer_public.parameters.into(),
+            vk: issuer_public.vk.iter().map(|vk| vk.into()).collect(),
+        }
+    }
+}
+
+/// VKCompressed: From<&types::VkCompressed>
+impl From<&types::VkCompressed> for VKCompressed {
+    fn from(vk: &types::VkCompressed) -> Self {
+        match vk {
+            types::VkCompressed::G1(g1) => VKCompressed::G1(g1.to_vec()),
+            types::VkCompressed::G2(g2) => VKCompressed::G2(g2.to_vec()),
+        }
+    }
+}
+
+/// ParamSetCommitmentCompressed: From<types::ParamSetCommitmentCompressed>
+impl From<types::ParamSetCommitmentCompressed> for ParamSetCommitmentCompressed {
+    fn from(param_set_commitment: types::ParamSetCommitmentCompressed) -> Self {
+        ParamSetCommitmentCompressed {
+            pp_commit_g1: param_set_commitment.pp_commit_g1,
+            pp_commit_g2: param_set_commitment.pp_commit_g2,
+        }
+    }
+}
+
+/// types::CredProofCompressed: From<&delanocreds::keypair::CredProofCompressed>
+impl From<&CredProofCompressed> for types::CredProofCompressed {
+    fn from(cred_proof: &CredProofCompressed) -> Self {
+        types::CredProofCompressed {
+            sigma: cred_proof.sigma.clone().into(),
+            commitment_vector: cred_proof.commitment_vector.clone(),
+            witness_pi: cred_proof.witness_pi.clone(),
+            nym_proof: cred_proof.nym_proof.clone().into(),
+        }
+    }
+}
+
+/// types::SignatureCompressed: From<delanocreds::keypair::SignatureCompressed>
+impl From<SignatureCompressed> for types::SignatureCompressed {
+    fn from(signature: SignatureCompressed) -> Self {
+        types::SignatureCompressed {
+            z: signature.z,
+            y_g1: signature.y_g1,
+            y_hat: signature.y_hat,
+            t: signature.t,
+        }
+    }
+}
+
+/// types::NymProofCompressed: From<delanocreds::keypair::NymProofCompressed>a
+impl From<NymProofCompressed> for types::NymProofCompressed {
+    fn from(nym_proof: NymProofCompressed) -> Self {
+        types::NymProofCompressed {
+            challenge: nym_proof.challenge,
+            pedersen_open: nym_proof.pedersen_open.clone().into(),
+            pedersen_commit: nym_proof.pedersen_commit,
+            public_key: nym_proof.public_key,
+            response: nym_proof.response,
+            damgard: nym_proof.damgard.clone().into(),
+        }
+    }
+}
+
+/// types::DamgardTransformCompressed: From<delanocreds::zkp::DamgardTransformCompressed>
+impl From<DamgardTransformCompressed> for types::DamgardTransformCompressed {
+    fn from(damgard: DamgardTransformCompressed) -> Self {
+        types::DamgardTransformCompressed {
+            pedersen: damgard.pedersen.clone().into(),
+        }
+    }
+}
+
+/// types::PedersenCompressed: From<delanocreds::zkp::PedersenCompressed>
+impl From<PedersenCompressed> for types::PedersenCompressed {
+    fn from(pedersen: PedersenCompressed) -> Self {
+        types::PedersenCompressed { h: pedersen.h() }
+    }
+}
+
+/// types::PedersenOpenCompressed: From<delanocreds::zkp::PedersenOpenCompressed>
+impl From<PedersenOpenCompressed> for types::PedersenOpenCompressed {
+    fn from(pedersen_open: PedersenOpenCompressed) -> Self {
+        types::PedersenOpenCompressed {
+            open_randomness: pedersen_open.open_randomness.clone().into(),
+            announce_randomness: pedersen_open.announce_randomness,
+            announce_element: pedersen_open.announce_element,
+        }
+    }
+}
+
+/// types::IssuerPublicCompressed: From<&delanocreds::keypair::IssuerPublicCompressed>
+impl From<&IssuerPublicCompressed> for types::IssuerPublicCompressed {
+    fn from(issuer_public: &IssuerPublicCompressed) -> Self {
+        types::IssuerPublicCompressed {
+            parameters: issuer_public.parameters.clone().into(),
+            vk: issuer_public.vk.iter().map(|vk| vk.into()).collect(),
+        }
+    }
+}
+
+/// types::ParamSetCommitmentCompressed: From<delanocreds::set_commits::ParamSetCommitmentCompressed>
+impl From<ParamSetCommitmentCompressed> for types::ParamSetCommitmentCompressed {
+    fn from(param_set_commitment: ParamSetCommitmentCompressed) -> Self {
+        types::ParamSetCommitmentCompressed {
+            pp_commit_g1: param_set_commitment.pp_commit_g1,
+            pp_commit_g2: param_set_commitment.pp_commit_g2,
+        }
+    }
+}
+
+/// types::VkCompressed: From<&delano_keys::vk::VKCompressed>
+impl From<&VKCompressed> for types::VkCompressed {
+    fn from(vk: &VKCompressed) -> Self {
+        match vk {
+            VKCompressed::G1(g1) => types::VkCompressed::G1(g1.to_vec()),
+            VKCompressed::G2(g2) => types::VkCompressed::G2(g2.to_vec()),
+        }
     }
 }
