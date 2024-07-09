cd1e124922126c5d027f5261441546d1d7a6d871
Doug Anderson444 <douganderson444@gmail.com>
2024-07-09 15:33:10 -0300

    fix comment (MaxCard amount)

--- crates/delanocreds/src/config.rs
+++ crates/delanocreds/src/config.rs
@@ -20,7 +20,8 @@
 
 /// Default Max Attributes: The maximum number of attribute entries allowed in a credential.
 pub const DEFAULT_MAX_ENTRIES: usize = 6;
-/// Default Max Cardinality: The maximum number of total attribute elements allowed in a credential. The Default os 12 is chosen as it is the maximum number tha will fit into an Issuer QR Code.
+
+/// Default Max Cardinality: The maximum number of total attribute elements allowed in a credential. The Default is 8 is chosen as it is the maximum number tha will fit into an Issuer QR Code.
 pub const DEFAULT_MAX_CARDINALITY: usize = 8;
 
 pub const CHALLENGE_STATE_NAME: &str = "schnorr";
